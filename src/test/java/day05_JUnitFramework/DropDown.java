e78d6ecef4cc861c0a0d51f8c10f9998160b0125
yusuf10-prog <kuzkayayusuf404@gmail.com>
2024-04-23 21:49:35 +0300

    yeni

--- src/test/java/day05_JUnitFramework/DropDown.java
+++ src/test/java/day05_JUnitFramework/DropDown.java
@@ -1,11 +1,14 @@
 package day05_JUnitFramework;
 
+import com.sun.nio.sctp.SendFailedNotification;
+import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
 import java.security.Key;
 import java.time.Duration;
@@ -39,6 +42,25 @@ public class DropDown {
         //driver.findElement(By.xpath("//*[@class='ui-state-default ui-corner-top ui-tabs-selected ui-state-active']")).click();
 
         driver.findElement(By.partialLinkText("Purchase Foreign Currency")).click();
+        WebElement selectButton= driver.findElement(By.xpath("//select[@id='pc_currency']"));
+        selectButton.click();
+
+        Select select=new Select(selectButton);
+        select.selectByVisibleText("Eurozone (euro)");
+        driver.findElement(By.xpath("//input[@id='pc_amount']")).sendKeys("23");
+        driver.findElement(By.xpath("(//input[@name='inDollars'])[2]")).click();
+
+        driver.findElement(By.xpath("//input[@id='pc_calculate_costs']")).click();
+        driver.findElement(By.xpath("//input[@value='Purchase']")).click();
+
+
+
+        String actual="Foreign currency cash was successfully purchased.";
+        WebElement expected=driver.findElement(By.xpath("//div[@id='alert_content']"));
+        String gelen=expected.getText();
+
+        Assert.assertEquals(actual,gelen);
+
         Thread.sleep(5000);
 
     }
